2cb19d548fc6b4bfc0442f25845ee740a697c6bf
Shau Yin Luk <ivan.luk@icloud.com>
2020-11-19 17:29:37 -0600

    set up base app

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import rootReducer from './reducers/manageUserAndReport';
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
