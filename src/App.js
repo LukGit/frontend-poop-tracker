d64700f9655ff843f2a50f1c30445a4549620862
Shau Yin Luk <ivan.luk@icloud.com>
2020-12-04 10:50:18 -0600

    Reports component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import ReportForm from './components/ReportForm'
+import Reports from './components/Reports'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path={'/login'} component={Login} />
         <Route path={'/signup'} component={Signup}/>
         <Route path={'/reports/new'} component={ReportForm} />
+        <Route path={'/reports'} component={Reports} />
         <Route path={'/'} component={Login} />
       </Switch>
       </BrowserRouter>
---
+++ src/components/Reports.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Navbar from './Navbar';
+import MapReports from './MapReports'
+import { Header, Icon } from 'semantic-ui-react'
+
+class Reports extends Component {
+
+  // this shows the NavBar and the MapReports which is also passed the report items to display on map
+  render() {
+    if (!this.props.user.user){
+      this.props.history.push('/login')
+      return null
+    }
+    return (
+      
+      <div className="courses">
+        <Navbar/>
+        <Header inverted size='medium'> 
+        <Icon name='bitbucket square'/>
+        {this.props.user.user}, 
+        {this.props.reports.length > 0 ? "Your Bucket List Courses!" : "No Course in Your Bucket!"}
+        </Header> 
+        <MapReports reports={this.props.reports}/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return { 
+    reports: state.reports,
+    user: state.users
+   }
+}
+
+export default connect(mapStateToProps)(Reports)
